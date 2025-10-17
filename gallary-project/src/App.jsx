3bfb6445ffff89a391b94dae3b60bb57d7bf1fcd
irtizapanjwani <scig26806@gmail.com>
2025-10-17 23:50:55 +0500

    setup axios tailwind

--- gallary-project/src/App.jsx
+++ gallary-project/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const getData = () => {
+    console.log("Button is clicked")
+  }
 
   return (
-    <>
-      <h1>Gallary Project</h1>
-    </>
+    <div className='bg-black h-screen p-4 text-white'>
+      <button onClick={getData} className="bg-green-600 px-6 py-3 rounded active:scale-95 text-white">Green</button>
+    </div>
   )
 }
 
